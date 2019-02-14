a4bb5b3ce50af7e2e1ebe72b15598e9a92597743
mralwin <owendthiele@gmail.com>
2019-02-14 17:00:17 -0500

    Added Modal
    Added form pupup

---
+++ src/components/AddExerciseForm.js
@@ -0,0 +1,47 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class AddExerciseForm extends React.Component {
+  nameRef = React.createRef();
+  setRef = React.createRef();
+  repRef = React.createRef();
+  restRef = React.createRef();
+  progressRef = React.createRef();
+
+  static propTypes = {
+    addExercise: PropTypes.func
+  };
+
+  createExercise = event => {
+    event.preventDefault();
+    const exercise = {
+      day: this.props.day,
+      name: this.nameRef.current.value,
+      set: this.setRef.current.value,
+      rep: this.repRef.current.value,
+      rest: this.restRef.current.value,
+      progress: this.progressRef.current.value
+    };
+    this.props.addexercise(exercise);
+    event.currentTarget.reset();
+  };
+  render() {
+    return (
+      <form onSubmit={this.createExercise}>
+        <input name="name" ref={this.nameRef} type="text" placeholder="Name" />
+        <input name="set" ref={this.setRef} type="number" placeholder="Sets" />
+        <input name="rep" ref={this.repRef} type="text" placeholder="Reps" />
+        <input name="rest" ref={this.restRef} type="text" placeholder="Rest" />
+        <input
+          name="progress"
+          ref={this.progressRef}
+          type="text"
+          placeholder="Weight increment"
+        />
+        <button type="submit">Add Exercise</button>
+      </form>
+    );
+  }
+}
+
+export default AddExerciseForm;
--- src/components/day.js
+++ src/components/day.js
@@ -1,10 +1,28 @@
 import React from "react";
+import Modal from "./Modal";
+import AddExerciseForm from "./AddExerciseForm";
 
 class Day extends React.Component {
+  state = { show: false };
+
+  showModal = () => {
+    this.setState({ show: true });
+  };
+
+  hideModal = () => {
+    this.setState({ show: false });
+  };
+
   render() {
     return (
       <div className="a">
         <h2>I am {this.props.index}</h2>
+        <Modal show={this.state.show} handleClose={this.hideModal}>
+          <AddExerciseForm />
+        </Modal>
+        <button type="button" onClick={this.showModal}>
+          open
+        </button>
       </div>
     );
   }
